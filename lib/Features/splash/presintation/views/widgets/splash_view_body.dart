11025d94a5dbca1fc1846f62f46c29c7446119a9
ahmedmsaaid <ahmedmsaaid908@gmail.com>
2024-11-07 08:51:25 +0200

    splash view

--- lib/Features/splash/presintation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presintation/views/widgets/splash_view_body.dart
@@ -1,10 +1,25 @@
+import 'package:bookly_app/core/utilis/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        logo(),
+      ],
+    );
   }
+}
+
+Widget logo() {
+  return SvgPicture.asset(
+    AssetsData.logo,
+    height: 75,
+  );
 }
