f5595f4d7f57506723e5ce0964590c1d85f0be01
ahmedmsaaid <ahmedmsaaid908@gmail.com>
2024-11-07 08:00:09 +0200

    create splash body

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'Features/splash/presintation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
